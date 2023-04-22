ebbbfcce3b172c88721de1928c8fd03a765cb3e4
coder_17 <tanupamsaha26@gmail.com>
2023-04-22 13:04:38 +0530

    major changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://decoder-17.github.io/edyoda/",
   plugins: [react()],
 });
